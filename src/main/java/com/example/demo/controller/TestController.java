c5e8a25aa394e78b83137dbfa6c5a9ee59b6b2c2
syhao <767884144@qq.com>
2022-05-04 10:11:11 +0800

    自动化部署项目提交

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+
+    @GetMapping("/selectToString")
+    public String selectToString() {
+        System.out.println("成功！");
+        return "成功！";
+    }
+
+}
